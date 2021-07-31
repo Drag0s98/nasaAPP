73a7ad43bfc8bfd452227a9817d2bfceb0532f34
Drag0s98 <dragostanase1698@gmail.com>
2021-07-31 18:11:35 +0200

    first get petition done!

--- controllers/landings.js
+++ controllers/landings.js
@@ -1,22 +1,34 @@
+const { parse } = require('dotenv');
+const { json } = require('express');
 const Landings = require('../models/landings')
 
 
 const landings = {
     homeLandings: async (req, res) => {
         let landings;
+        //console.log(req.query.minimun_mass);
+        let minimun_mass = parseInt(req.query.minimun_mass)
+        console.log(minimun_mass);
+        try {
+            landings = await Landings.find()
+            let selected = landings.map((param) =>{
+                let mass = parseInt(param.mass)
+                let name = param.name
+              //  console.log(mass);
+                if(mass >= minimun_mass){
+                    return "The names is " + name +"and mass is: " +  mass
+                }
+            })
+            let filtered = selected.filter((param)=>{return param != null})
+            
 
-        try{
-            req.params.mass ? 
-                landings = await Landings.find({
-                    name: String(req.params.mass)
-                }) : 
-                landings = await Landings.find()
-                console.log(landings);
-
-                res.status(200).json({
-                    landings
-                });
-        }catch(error){
+            
+            
+            
+            res.status(200).send(
+                filtered
+            );
+        } catch (error) {
             res.status(400).json({
                 "error": error.message
             })
--- routes/landings.js
+++ routes/landings.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const pages = require('../controllers/landings')
 
 
-router.get('/api/astronomy/landings:mass?', pages.homeLandings)
+router.get('/api/astronomy/landings:minimun_mass?', pages.homeLandings)
 
 module.exports = router
