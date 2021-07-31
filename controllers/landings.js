5a657e1102ebafbe527299b262ceb2fbb0e0c90f
Drag0s98 <dragostanase1698@gmail.com>
2021-07-31 18:45:19 +0200

    3 of 4 get's done, get by mass and class its finished

--- controllers/landings.js
+++ controllers/landings.js
@@ -11,23 +11,62 @@ const landings = {
         console.log(minimun_mass);
         try {
             landings = await Landings.find()
-            let selected = landings.map((param) =>{
+            let selected = landings.map((param) => {
                 let mass = parseInt(param.mass)
                 let name = param.name
-              //  console.log(mass);
-                if(mass >= minimun_mass){
-                    return "The names is " + name +"and mass is: " +  mass
+                //  console.log(mass);
+                if (mass >= minimun_mass) {
+                    return "The names is " + name + "and mass is: " + mass
                 }
             })
-            let filtered = selected.filter((param)=>{return param != null})
-            
-
-            
-            
-            
+            let filtered = selected.filter((param) => { return param != null })
             res.status(200).send(
                 filtered
             );
+        } catch (error) {
+            res.status(400).json({
+                "error": error.message
+            })
+        }
+    },
+    specificMass: async (req, res) => {
+        let landings;
+        let takeMass = parseInt(req.params.mass)
+        try {
+            landings = await Landings.find()
+            let array = await landings.map((param) => {
+                let mass = parseInt(param.mass)
+                let name = param.name
+
+                if (mass === takeMass) {
+                    return "The names is " + name + "and mass is: " + mass
+                }
+            })
+            let filtered = array.filter((param) => { return param != null })
+
+            res.status(200).send(filtered)
+        } catch (error) {
+            res.status(400).json({
+                "error": error.message
+            })
+        }
+    },
+    byClass: async (req, res) => {
+        let landings
+        let takeClass = req.params.class
+
+        try {
+            landings = await Landings.find()
+            let array = await landings.map((param) => {
+                let classes = param.recclass
+                let name = param.name
+                console.log(takeClass);
+                if(classes === takeClass){
+                    return "The names is " + name + "and the class is: " + classes
+                }
+            })
+            let filtered = array.filter((param) => { return param != null })
+            res.status(200).send(filtered)
         } catch (error) {
             res.status(400).json({
                 "error": error.message
--- routes/landings.js
+++ routes/landings.js
@@ -3,5 +3,7 @@ const pages = require('../controllers/landings')
 
 
 router.get('/api/astronomy/landings:minimun_mass?', pages.homeLandings)
+router.get('/api/astronomy/landings/mass/:mass?', pages.specificMass)
+router.get('/astronomy/landings/class/:class?', pages.byClass)
 
 module.exports = router
