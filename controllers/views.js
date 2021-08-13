a4bff167f9bcd0431e426ebb630520a381af1fd0
Drag0s98 <dragostanase1698@gmail.com>
2021-08-13 14:31:20 +0200

    sign in of users ready

--- controllers/views.js
+++ controllers/views.js
@@ -1,8 +1,11 @@
 const fetch = require('node-fetch')
 const Landings = require('../models/landings')
 const jsStringify = require('js-stringify')
+const Users = require('../models/users')
+
 
 const pages = {
+
     home: (req, res) => {
         fetch('https://api.nasa.gov/planetary/apod?api_key=jSRbRCxea7XZD7bfyqYTbocXO1rCAvbSyp06Nlzc')
             .then(res => res.json())
@@ -36,15 +39,41 @@ const pages = {
 
     },
     register: async (req, res) => {
-        try{
+        try {
             let msj = 'Pagina de registro'
             res.status(200).render('register', { msj })
-        }catch(error){
+        } catch (error) {
             res.status(400).send(error)
         }
-        
+
+    },
+    sendRegister: async (req, res) => {
+        console.log(req.body);
+        const regName = /[A-Z]{1}[a-z]+$/
+        const regbirthDate = /([0-9]{2})\/([0-9]{2})\/([0-9]{4})/
+        const regAffNum = /[0-9]{5}/
+        let errMsj = 'Invalid formulary, please try again'
+        try {
+            if (regName.test(req.body.name) === true && regbirthDate.test(req.body.birthdate) === true && regAffNum.test(req.body.affiliatedNumber) === true) {
+                console.log("*****User created*****");
+                let user = new Users(req.body)
+                const new_user = await user.save()
+                console.log(new_user);
+
+                let users = await Users.find()
+                let data = [new_user, {
+                    "users_before": users,
+                }]
+                res.status(201).json(data)
+            } else {
+                res.status(200).render('register', { errMsj })
+            }
+        } catch (error) {
+            res.status(400).send('Ha ocurrido un error: ' + error)
+        }
+
     }
-   
+
 
 
 
