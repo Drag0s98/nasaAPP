91f35f350b663be53dd9ea81b33a0ffeb72c511a
Drag0s98 <dragostanase1698@gmail.com>
2021-08-19 12:46:02 +0200

    style added

--- routes/landings.js
+++ routes/landings.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const pages = require('../controllers/landings')
 
+router.get('/landings', pages.base)
 router.get('/landings:minimum_mass?:from?:to?', pages.getAllLandings)
 router.get('/landings/mass/:mass?', pages.specificMass)
 router.get('/landings/class/:class?', pages.byClass)
